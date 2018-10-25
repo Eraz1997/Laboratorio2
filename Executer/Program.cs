1f75d0933cfc67c85f716c959cef364a622162ca
Eraz1997 <4341657@studenti.unige.it>
2018-10-25 12:26:39 +0200

    release 1.0

--- Executer/Program.cs
+++ Executer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using MyAttribute;
 
 namespace Executer
 {
@@ -10,6 +12,32 @@ namespace Executer
     {
         static void Main(string[] args)
         {
+            var a = Assembly.LoadFrom("MyLibrary.dll");
+            foreach(var type in a.GetTypes())
+            {
+                if (type.IsClass)
+                {
+                    Console.WriteLine(type.FullName);
+                    Console.ReadLine();
+                }
+            }
+
+            foreach (var type in a.GetTypes())
+            {
+                if (!type.IsClass)
+                    continue;
+                MethodInfo[] members = type.GetMethods();
+                foreach (var member in members)
+                {
+                    object instance = Activator.CreateInstance(type);
+                    ExecuteMe []attributes = (ExecuteMe[]) member.GetCustomAttributes(typeof(ExecuteMe), false);
+                    foreach(var attribute in attributes)
+                    {
+                        member.Invoke(instance, attribute.Values);
+                        Console.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
---
+++ MyAttribute/ExecuteMe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyAttribute
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class ExecuteMe : Attribute
+    {
+        public object [] Values { get; private set; }
+
+        public ExecuteMe(params object [] valObjects)
+        {
+            Values = valObjects;
+        }
+
+    }
+}
